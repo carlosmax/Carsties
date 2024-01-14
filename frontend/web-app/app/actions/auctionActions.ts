bb341b47d58b71632b91556b0e507583097def86
Carlos Max <carlosmax.dev@hotmail.com>
2024-01-14 11:28:22 -0300

    add production related changes

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -2,7 +2,7 @@
 
 import { PagedResult, Auction, Bid } from "@/types"
 import { getTokenWorkaround } from "./authActions"
-import { fetchWrapper } from "@/lib/fetchWrapper"
+import { fetchWrapper } from "@/app/lib/fetchWrapper"
 import { FieldValues } from "react-hook-form"
 import { revalidatePath } from "next/cache"
 
--- frontend/web-app/providers/SignalRProvider.tsx
+++ frontend/web-app/providers/SignalRProvider.tsx
@@ -20,15 +20,14 @@ export default function SignalRProvider({ user, children }: Props) {
   const [connection, setConnection] = useState<HubConnection | null>(null)
   const setCurrentPrice = useAuctionStore((state) => state.setCurrentPrice)
   const addBid = useBidStore((state) => state.addBid)
+  const apiUrl =
+    process.env.NODE_ENV === "production" ? "https://api.carsties/notifications" : process.env.NEXT_PUBLIC_NOTIFY_URL
 
   useEffect(() => {
-    const newConnection = new HubConnectionBuilder()
-      .withUrl("http://localhost:6001/notifications")
-      .withAutomaticReconnect()
-      .build()
+    const newConnection = new HubConnectionBuilder().withUrl(apiUrl!).withAutomaticReconnect().build()
 
     setConnection(newConnection)
-  }, [])
+  }, [apiUrl])
 
   useEffect(() => {
     if (connection) {
